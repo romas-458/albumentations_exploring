84d72dabec897aaf8946245683892e09fc37200d
roman <roman.maslii@peoly.com>
2021-10-01 18:17:01 +0300

    add transform_default2

--- set_transforms.py
+++ set_transforms.py
@@ -48,6 +48,28 @@ data_transforms_default = {
     ),
 }
 
+data_transforms_default2 = {
+    'train': A.Compose(
+        [
+            A.SmallestMaxSize(max_size=160),
+            A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
+            # A.RandomCrop(height=128, width=128),
+            A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
+            A.RandomBrightnessContrast(p=0.5),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(),
+        ]
+    ),
+    'val': A.Compose(
+        [
+            A.SmallestMaxSize(max_size=160),
+            A.CenterCrop(height=128, width=128),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(),
+        ]
+    ),
+}
+
 data_transforms_cube = {
     'train': A.Compose(
         [
