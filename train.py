39b29038246a01d1369d70695b22dad178fe536e
roman <roman.maslii@peoly.com>
2021-10-23 13:19:33 +0300

    add train_model_wandb_log function

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import time
 import copy
 import matplotlib.pyplot as plt
 import numpy as np
+import wandb
 
 def train_model(model, criterion, optimizer, scheduler, dataloaders, device, dataset_sizes, num_epochs=25):
     since = time.time()
@@ -58,6 +59,106 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, device, dat
 
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            if phase == 'train':
+                train_loss.append(epoch_loss)
+                train_acc.append(epoch_acc)
+            else:
+                val_loss.append(epoch_loss)
+                val_acc.append(epoch_acc)
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+                torch.save(best_model_wts, 'resnet.pt')
+                print('Improvement-Detected, save-model')
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    x = np.arange(num_epochs)
+    plt.subplot(221)
+    plt.plot(x, train_loss, c='red', label='train loss')
+    plt.plot(x, val_loss, c='blue', label='val loss')
+    plt.legend(loc='best')
+
+    plt.subplot(222)
+    plt.plot(x, train_acc, c='red', label='train acc')
+    plt.plot(x, val_acc, c='blue', label='val acc')
+    plt.legend(loc='best')
+
+    plt.show()
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+def train_model_wandb_log(model, criterion, optimizer, scheduler, dataloaders, device, dataset_sizes, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    train_loss = []
+    val_loss = []
+    train_acc = []
+    val_acc = []
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels, _ in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            if phase == 'train':
+                wandb.log({'phase': phase, 'epoch_loss_train': epoch_loss, 'epoch_acc_train': epoch_acc})
+            if phase == 'val':
+                wandb.log({'phase': phase, 'epoch_loss_val': epoch_loss, 'epoch_acc_val': epoch_acc})
 
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
